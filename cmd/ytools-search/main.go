f21157097c31a7e3a31d9fc05fc440d4abb09f90
codesoap <codesoap@mailbox.org>
2020-07-04 11:59:25 +0200

    Adapt ytools-search to new HTML

--- cmd/ytools-search/main.go
+++ cmd/ytools-search/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
 	"github.com/codesoap/ytools"
-	"golang.org/x/net/html"
+	"io"
+	"io/ioutil"
 	"net/http"
 	"net/url"
 	"os"
+	"regexp"
 	"strings"
 )
 
@@ -17,10 +21,75 @@ type Video struct {
 	Url   string
 }
 
+// TODO: Consider using nested structs to save some lines of code.
+
+type YtInitialData struct {
+	Contents YtInitialDataContents
+}
+
+type YtInitialDataContents struct {
+	TwoColumnSearchResultsRenderer TwoColumnSearchResultsRenderer
+}
+
+type TwoColumnSearchResultsRenderer struct {
+	PrimaryContents PrimaryContents
+}
+
+type PrimaryContents struct {
+	SectionListRenderer SectionListRenderer
+}
+
+type SectionListRenderer struct {
+	Contents []SectionListRendererContent
+}
+
+type SectionListRendererContent struct {
+	ItemSectionRenderer ItemSectionRenderer
+}
+
+type ItemSectionRenderer struct {
+	Contents []ItemSectionRendererContent
+}
+
+type ItemSectionRendererContent struct {
+	// ShelfRenderer ShelfRenderer
+	VideoRenderer VideoRenderer
+}
+
+// type ShelfRenderer struct {
+// 	Content ShelfRendererContent
+// }
+
+// type ShelfRendererContent struct {
+// 	VerticalListRenderer VerticalListRenderer
+// }
+
+// type VerticalListRenderer struct {
+// 	Items []VerticalListRendererItem
+// }
+
+// type VerticalListRendererItem struct {
+// 	VideoRenderer VideoRenderer
+// }
+
+type VideoRenderer struct {
+	VideoId string
+	Title   VideoRendererTitle
+}
+
+type VideoRendererTitle struct {
+	Runs []Run
+}
+
+type Run struct {
+	Text string
+}
+
 func main() {
 	search_url := get_search_url()
 	videos, err := scrape_off_videos(search_url)
 	if err != nil {
+		fmt.Fprintf(os.Stderr, err.Error())
 		os.Exit(1)
 	}
 	if len(videos) == 0 {
@@ -46,34 +115,77 @@ func get_search_url() string {
 }
 
 func scrape_off_videos(search_url string) (videos []Video, err error) {
-	videos = make([]Video, 0, max_results)
-
 	resp, err := http.Get(search_url)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Could not get '%s'\n", search_url)
 		return
 	}
 	defer resp.Body.Close()
+	dataJson, err := extract_json(resp.Body)
+	if err != nil {
+		return
+	}
+	return extract_videos(dataJson)
+}
 
-	tokenizer := html.NewTokenizer(resp.Body)
-	for {
-		switch tokenizer.Next() {
-		case html.ErrorToken:
-			return
-		case html.StartTagToken:
-			token := tokenizer.Token()
-			if is_video_title_link(token) {
-				video, ok := extract_video_from_title_link(token)
-				if !ok {
-					return
-				}
-				videos = append(videos, video)
-				if len(videos) == max_results {
-					return
-				}
+func extract_json(body io.Reader) (mainJson []byte, err error) {
+	bytes, err := ioutil.ReadAll(body)
+	if err != nil {
+		return
+	}
+	re := regexp.MustCompile(`(?m)^ *window\["ytInitialData"\] *= *(.*); *$`)
+	matches := re.FindSubmatch(bytes)
+	if matches == nil {
+		err = errors.New("retrieved HTML does not contain the expected JSON")
+		return
+	}
+	return matches[1], nil
+}
+
+func extract_videos(dataJson []byte) (videos []Video, err error) {
+	videos = make([]Video, 0, max_results)
+	var data YtInitialData
+	if err = json.Unmarshal(dataJson, &data); err != nil {
+		return
+	}
+
+	pc := data.Contents.TwoColumnSearchResultsRenderer.PrimaryContents
+	for _, slrContent := range pc.SectionListRenderer.Contents {
+		for _, isrContent := range slrContent.ItemSectionRenderer.Contents {
+			var video Video
+			video, err = extract_video_from_video_renderer(isrContent.VideoRenderer)
+			if err != nil {
+				// This sometimes happens, but I don't think it's problematic.
+				err = nil
+				continue
+			}
+			videos = append(videos, video)
+			if len(videos) == max_results {
+				break
 			}
 		}
 	}
+	return
+}
+
+func extract_video_from_video_renderer(renderer VideoRenderer) (video Video, err error) {
+	if len(renderer.VideoId) == 0 {
+		err = errors.New("videoId is missing in videoRenderer")
+		return
+	}
+	if len(renderer.Title.Runs) != 1 {
+		err = errors.New("multiple or no runs found for videoRenderer")
+		return
+	}
+	if len(renderer.Title.Runs[0].Text) == 0 {
+		err = errors.New("no title found for videoRenderer")
+		return
+	}
+	video = Video{
+		Url: fmt.Sprintf("https://www.youtube.com/watch?v=%s", renderer.VideoId),
+		Title: renderer.Title.Runs[0].Text,
+	}
+	return
 }
 
 func save_videos_urls(videos []Video) (err error) {
@@ -88,36 +200,4 @@ func print_video_titles(videos []Video) {
 	for i, video := range videos {
 		fmt.Printf("%2d: %s\n", i+1, video.Title)
 	}
-}
-
-func is_video_title_link(token html.Token) bool {
-	is_tile_link, is_video := false, false
-	if token.Data == "a" {
-		for _, a := range token.Attr {
-			if a.Key == "class" && strings.Contains(a.Val, "yt-uix-tile-link") {
-				is_tile_link = true
-			}
-			// This filters out channels and playlists:
-			if a.Key == "href" && strings.HasPrefix(a.Val, "/watch") &&
-				len(a.Val) == 20 {
-				is_video = true
-			}
-		}
-	}
-	return is_tile_link && is_video
-}
-
-func extract_video_from_title_link(token html.Token) (video Video, ok bool) {
-	for _, a := range token.Attr {
-		if a.Key == "title" {
-			video.Title = a.Val
-		}
-		if a.Key == "href" {
-			video.Url = fmt.Sprintf("https://www.youtube.com%s", a.Val)
-		}
-	}
-	if video.Title != "" && video.Url != "" {
-		ok = true
-	}
-	return
 }
