2fc52a9074f384dfd5fa6e9d6da1037b73873f6e
codesoap <codesoap@mailbox.org>
2020-08-08 11:31:48 +0200

    Adapt ytools-info to new HTML

--- cmd/ytools-info/main.go
+++ cmd/ytools-info/main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
 	"github.com/codesoap/ytools"
-	"golang.org/x/net/html"
-	"net/http"
 	"os"
 	"strings"
 )
@@ -22,6 +22,61 @@ type Info struct {
 	Description string
 }
 
+type YtInitialData struct {
+	Contents struct {
+		TwoColumnWatchNextResults struct {
+			Results struct {
+				Results struct {
+					Contents []struct {
+						VideoPrimaryInfoRenderer   VideoPrimaryInfoRenderer
+						VideoSecondaryInfoRenderer VideoSecondaryInfoRenderer
+					}
+				}
+			}
+		}
+	}
+}
+
+type VideoPrimaryInfoRenderer struct {
+	Title struct {
+		Runs []struct {
+			Text string
+		}
+	}
+	ViewCount struct {
+		VideoViewCountRenderer struct {
+			ViewCount struct {
+				SimpleText string
+			}
+		}
+	}
+	SentimentBar struct {
+		SentimentBarRenderer struct {
+			Tooltip string
+		}
+	}
+	DateText struct {
+		SimpleText string
+	}
+}
+
+type VideoSecondaryInfoRenderer struct {
+	Owner struct {
+		VideoOwnerRenderer struct {
+			Title struct {
+				Runs []struct {
+					Text string
+				}
+			}
+		}
+	}
+	Description struct {
+		Runs []struct {
+			Text string
+		}
+	}
+}
+
 func main() {
 	video_url, err := ytools.GetDesiredVideoUrl()
 	if err != nil {
@@ -37,165 +92,89 @@ func main() {
 
 func print_info(info Info) {
 	fmt.Println(info.Title)
-	fmt.Printf("%s  ▲ %s  ▼ %s  %s\n\n", info.Views, info.Likes, info.Dislikes,
-		info.Date)
+	f := "%s  ▲ %s  ▼ %s  %s\n\n"
+	fmt.Printf(f, info.Views, info.Likes, info.Dislikes, info.Date)
 	fmt.Println(info.Description)
 }
 
 func scrape_off_info(url string) (info Info, err error) {
-	resp, err := http.Get(url)
-	if err != nil {
+	var dataJson []byte
+	if dataJson, err = ytools.ExtractJson(url); err != nil {
 		return
 	}
-	defer resp.Body.Close()
+	return extract_info(dataJson)
+}
 
-	tokenizer := html.NewTokenizer(resp.Body)
-	for {
-		switch tokenizer.Next() {
-		case html.ErrorToken:
-			return
-		case html.StartTagToken:
-			err = fill_in_available_info(tokenizer, &info)
-			if err != nil {
-				return
-			}
-		}
+func extract_info(dataJson []byte) (info Info, err error) {
+	var data YtInitialData
+	if err = json.Unmarshal(dataJson, &data); err != nil {
+		return
 	}
-	if !info_complete(info) {
-		err = fmt.Errorf("missing some info field(s)")
+	r := data.Contents.TwoColumnWatchNextResults.Results.Results
+	if len(r.Contents) == 0 {
+		return info, errors.New("no contents found in JSON")
 	}
+	primaryInfo := r.Contents[0].VideoPrimaryInfoRenderer
+	secondaryInfo := r.Contents[1].VideoSecondaryInfoRenderer
+	if err = fill_title(&info, primaryInfo); err != nil {
+		return
+	}
+	if err = fill_views(&info, primaryInfo); err != nil {
+		return
+	}
+	if err = fill_votes(&info, primaryInfo); err != nil {
+		return
+	}
+	if err = fill_date(&info, primaryInfo); err != nil {
+		return
+	}
+	// TODO: Owner
+	fill_description(&info, secondaryInfo)
 	return
 }
 
-func fill_in_available_info(tokenizer *html.Tokenizer, info *Info) (err error) {
-	ok := true
-	token := tokenizer.Token()
-	switch {
-	case is_title(token):
-		info.Title, ok = extract_next_text(tokenizer)
-	case is_views(token):
-		info.Views, ok = extract_next_text(tokenizer)
-	case is_likes(token):
-		info.Likes, ok = extract_next_text(tokenizer)
-	case is_dislikes(token):
-		info.Dislikes, ok = extract_next_text(tokenizer)
-	case is_date(token):
-		info.Date, ok = extract_next_text(tokenizer)
-	case is_description(token):
-		info.Description, ok = extract_description(tokenizer)
+func fill_title(info *Info, data VideoPrimaryInfoRenderer) error {
+	if len(data.Title.Runs) != 1 {
+		return errors.New("no or multiple titles found in JSON")
 	}
-	if !ok {
-		err = fmt.Errorf("failed at extraction")
+	info.Title = data.Title.Runs[0].Text
+	if len(info.Title) == 0 {
+		return errors.New("title is empty")
 	}
-	return
+	return nil
 }
 
-func is_title(token html.Token) bool {
-	for _, a := range token.Attr {
-		if a.Key == "class" && a.Val == "watch-title" {
-			return true
-		}
+func fill_views(info *Info, data VideoPrimaryInfoRenderer) error {
+	info.Views = data.ViewCount.VideoViewCountRenderer.ViewCount.SimpleText
+	if len(info.Views) == 0 {
+		return errors.New("views is empty")
 	}
-	return false
+	return nil
 }
 
-func is_views(token html.Token) bool {
-	for _, a := range token.Attr {
-		if a.Key == "class" && a.Val == "watch-view-count" {
-			return true
-		}
+func fill_votes(info *Info, data VideoPrimaryInfoRenderer) error {
+	tooltip := data.SentimentBar.SentimentBarRenderer.Tooltip
+	numbers := strings.Split(tooltip, " / ")
+	if len(numbers) != 2 {
+		return errors.New("could not parse likes and dislikes")
 	}
-	return false
+	info.Likes = numbers[0]
+	info.Dislikes = numbers[1]
+	return nil
 }
 
-// FIXME: Results are off by one
-func is_likes(token html.Token) bool {
-	for _, a := range token.Attr {
-		if a.Key == "class" &&
-			strings.Contains(a.Val, "like-button-renderer-like-button") {
-			return true
-		}
+func fill_date(info *Info, data VideoPrimaryInfoRenderer) error {
+	info.Date = data.DateText.SimpleText
+	if len(info.Date) == 0 {
+		return errors.New("date is empty")
 	}
-	return false
+	return nil
 }
 
-// FIXME: Results are off by one
-func is_dislikes(token html.Token) bool {
-	for _, a := range token.Attr {
-		if a.Key == "class" &&
-			strings.Contains(a.Val, "like-button-renderer-dislike-button") {
-			return true
-		}
+func fill_description(info *Info, data VideoSecondaryInfoRenderer) {
+	desc := []byte("")
+	for _, run := range data.Description.Runs {
+		desc = append(desc, []byte(run.Text)...)
 	}
-	return false
-}
-
-func is_date(token html.Token) bool {
-	for _, a := range token.Attr {
-		if a.Key == "class" && a.Val == "watch-time-text" {
-			return true
-		}
-	}
-	return false
-}
-
-func is_description(token html.Token) bool {
-	for _, a := range token.Attr {
-		if a.Key == "id" && a.Val == "eow-description" {
-			return true
-		}
-	}
-	return false
-}
-
-func extract_description(tokenizer *html.Tokenizer) (desc string, ok bool) {
-	description := make([]byte, 0, 100)
-	for {
-		switch tokenizer.Next() {
-		case html.ErrorToken:
-			return
-		case html.TextToken:
-			description = append(description, tokenizer.Text()...)
-		case html.SelfClosingTagToken:
-			if tokenizer.Token().Data == "br" {
-				description = append(description, '\n')
-			}
-		case html.StartTagToken:
-			if tokenizer.Token().Data == "a" {
-				var next_text string
-				next_text, ok = extract_next_text(tokenizer)
-				if !ok {
-					return
-				}
-				description = append(description, []byte(next_text)...)
-			}
-		case html.EndTagToken:
-			if tokenizer.Token().Data == "p" {
-				return string(description), true
-			}
-		}
-	}
-}
-
-func extract_next_text(tokenizer *html.Tokenizer) (text string, ok bool) {
-	for {
-		switch tokenizer.Next() {
-		case html.ErrorToken:
-			return
-		case html.TextToken:
-			raw_text := tokenizer.Text()
-			tmp_text := make([]byte, len(raw_text))
-			copy(tmp_text, raw_text)
-			return strings.TrimSpace(string(tmp_text)), true
-		}
-	}
-}
-
-func info_complete(info Info) bool {
-	return info.Title != "" &&
-		info.Views != "" &&
-		info.Likes != "" &&
-		info.Dislikes != "" &&
-		info.Date != "" &&
-		info.Description != ""
+	info.Description = string(desc)
 }
